8ff27506fc2290b8badb70cf972aede724b7166c
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2017-06-23 19:04:07 +0900

    Added main.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-/*global require*/
-(function (r) {
-    "use strict";
-    r("coffee-script/register");
-    r("./gulpfile.coffee");
-}(require));
+((require) => {
+  const g = require('gulp');
+  const lint = require('gulp-eslint');
+  const notify = require('gulp-notify');
+  const plumber = require('gulp-plumber');
+
+  g.task('compile', () => {
+    g.src('lib/**/*.js').pipe(
+      plumber({ errorHandler: notify.onError('<%= error.message %>') })
+    ).pipe(lint());
+  });
+  g.task('default', () => g.watch('./src/**/*.coffee', ['compile']));
+})(require);
