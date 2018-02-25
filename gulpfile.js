d135a701ca326fa11b0b3fa7da875f13a1d643a1
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2018-02-25 16:37:51 +0900

    Needs gulp plugin for nyc... :x:

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,30 @@
   const lint = require('gulp-eslint');
   const notify = require('gulp-notify');
   const plumber = require('gulp-plumber');
+  const istanbul = require('gulp-istanbul');
+  const mocha = require('gulp-mocha');
 
-  g.task('compile', () => {
-    g.src('lib/**/*.js').pipe(
-      plumber({ errorHandler: notify.onError('<%= error.message %>') })
-    ).pipe(lint()).pipe(lint.format())
-      .pipe(lint.failAfterError());
-  });
-  g.task('default', () => g.watch('lib/**/*.js', ['compile']));
+  g.task(
+    'coverage.hook', () => g.src('lib/**/*.js')
+      .pipe(istanbul())
+      .pipe(istanbul.hookRequire())
+  );
+
+  g.task(
+    'test', () => g.src('tests/**/test_*.js')
+      .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
+      .pipe(mocha())
+      .pipe(istanbul.writeReports())
+      .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
+  );
+
+  g.task('compile', () => g.src('lib/**/*.js')
+    .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
+    .pipe(lint())
+    .pipe(lint.format())
+    .pipe(lint.failAfterError()));
+
+  g.task('default', () => g.watch(['lib/**/*.js', 'tests/**/*.js'], g.series(
+    'coverage.hook', 'test', 'compile'
+  )));
 })(require);
---
+++ tests/test_coffee.js
@@ -0,0 +1,12 @@
+/* global describe, it */
+((r) => {
+  const Undertaker = r('undertaker');
+  const { expect } = r('chai');
+  const Coffee = r('../lib/coffee');
+  describe('Coffeescript task check for normal init', () => {
+    it("Has 'coffee' task", () => {
+      const task = (new Undertaker(new Coffee('src'))).task('coffee');
+      expect(task).to.be.ok;
+    });
+  });
+})(require);
