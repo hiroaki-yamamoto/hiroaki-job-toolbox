e3cfc4649f869a8da610b902421ba6998860de61
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2018-02-26 15:53:01 +0900

    Added nyc run command instead, but I don't like it because it can't be managed by gulp :(

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,6 @@
   const lint = require('gulp-eslint');
   const notify = require('gulp-notify');
   const plumber = require('gulp-plumber');
-  const istanbul = require('gulp-istanbul');
-  const mocha = require('gulp-mocha');
-
-  g.task(
-    'coverage.hook', () => g.src('lib/**/*.js')
-      .pipe(istanbul())
-      .pipe(istanbul.hookRequire())
-  );
-
-  g.task(
-    'test', () => g.src('tests/**/test_*.js')
-      .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
-      .pipe(mocha())
-      .pipe(istanbul.writeReports())
-      .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
-  );
 
   g.task('compile', () => g.src('lib/**/*.js')
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
@@ -27,6 +11,6 @@
     .pipe(lint.failAfterError()));
 
   g.task('default', () => g.watch(['lib/**/*.js', 'tests/**/*.js'], g.series(
-    'coverage.hook', 'test', 'compile'
+    'compile'
   )));
 })(require);
