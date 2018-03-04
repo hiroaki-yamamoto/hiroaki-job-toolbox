72b42e47509d6535a348e6ac92122e242c41444f
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2018-03-04 10:12:01 +0900

    Finished adding coffeetest

--- tests/test_coffee.js
+++ tests/test_coffee.js
@@ -1,38 +1,49 @@
 /* global describe, it, beforeEach, afterEach */
 ((r) => {
   const gulp = r('gulp');
-  const Undertaker = r('undertaker');
   const rimraf = r('rimraf');
   const { expect } = r('chai');
   const path = r('path');
   const Coffee = r('../lib/coffee');
+  const helper = r('../lib/helper');
 
-  describe('Coffeescript task check for normal init', () => {
-    it('Has "coffee" task', () => {
-      const task = (new Undertaker(new Coffee('src', 'dest'))).task('coffee');
-      return expect(task).to.be.ok;
-    });
-  });
-
-  describe('The task should actually work', () => {
+  describe('General setting test', () => {
     const outPath = 'tests/build/coffee/assets.js';
+    let task;
     beforeEach(() => {
       rimraf.sync(path.dirname(outPath));
-      gulp.registry(
-        new Coffee('tests/fixtures/test_coffee/*.coffee', outPath)
-      );
+      task = new Coffee('tests/fixtures/test_coffee/*.coffee', outPath);
+      gulp.registry(task);
       gulp.on('error', (err) => { throw err; });
     });
-    afterEach(() => {});
-    // afterEach(() => {
-    //   rimraf.sync(outPath);
-    // });
+    afterEach(() => {
+      rimraf.sync(outPath);
+    });
+    it('Check if the option is default setting', () => {
+      expect(task.opts).to.eql({
+        lintCfg: path.resolve(path.join(__dirname, '../etc/coffeelint.json')),
+        isProductionMode: helper.isProduction,
+      });
+    });
     it('Check code generation', (done) => {
       gulp.series('coffee')((err) => {
         if (err) done(err);
         const assets = r('./build/coffee/assets.js');
         expect(assets.add(1, 2)).to.be.equal(3);
         done();
+      });
+    });
+  });
+
+  describe('Setting with un-default.', () => {
+    let task;
+    beforeEach(() => {
+      task = new Coffee('src', 'dest', { isProductionMode: true });
+    });
+    it('Check if the option is correct', () => {
+      expect(task.opts).to.eql({
+        lintCfg: path.resolve(path.join(__dirname, '../etc/coffeelint.json')),
+        isProductionMode: true,
       });
     });
   });
